a6c32d789b7198b759dbb6fb2fd71cac1df4e215
SH13LDS <coalshields@gmail.com>
2020-08-15 21:29:27 -0500

    Pass user data in via props

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,40 +2,10 @@ import React, { Component } from 'react';
 import UserItem from './UserItem';
 
 class Users extends Component {
-  state = {
-    users: [
-      {
-        id: '13',
-        login: 'sh13lds',
-        avatar_url:
-          'https://avatars2.githubusercontent.com/u/58605089?s=460&u=c188315f7eaa993a34e73f6ba4c91fb0d63229b0&v=4',
-        html_url: 'https://github.com/sh13lds',
-      },
-      {
-        id: '1',
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo',
-      },
-      {
-        id: '2',
-        login: 'defunkt',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-        html_url: 'https://github.com/defunkt',
-      },
-      {
-        id: '3',
-        login: 'pjhyett',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
-        html_url: 'https://github.com/pjhyett',
-      },
-    ],
-  };
-
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map((user) => (
+        {this.props.users.map((user) => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
